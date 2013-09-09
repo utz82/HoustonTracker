779725bc0eee4d8e0efff976a783df2494496a7c
utz82 <ipcore@server.fake>
2013-09-09 13:42:37 +0200

    improved build routine

--- as.bat
+++ as.bat
@@ -1,8 +1,52 @@
 
 cd ..
 @echo off
-if %target% == ti82 crasm Housto~1\ti82\main
-if %target% == ti83 asm83 Housto~1\ti83\main
-if %target% == ti8xp asm Housto~1\ti8xp\main
-if %target% == ti73 duck Housto~1\ti73\main
+if %target% == ti82 goto 82P
+if %target% == ti83 goto 83P
+if %target% == ti8xp goto 8XP
+if %target% == ti73 goto 73P
 pause
+
+:82P
+tasm -80 -B -Q -R16 Housto~1\ti82\main.asm houston.obj
+if errorlevel 1 goto ERRORS
+CRPRGM82 HOUSTON.OBJ
+del HOUSTON.OBJ >nul
+del Housto~1\ti82\main.lst >nul
+goto DDONE
+
+:73P
+tasm -80 -i -b Housto~1\ti73\main.asm houston.bin
+if errorlevel 1 goto ERRORS
+devpac83 houston
+del houston.73p > nul
+asm73 houston.83p
+goto DONE
+
+:8XP
+echo #define TI83P >temp.z80
+type Housto~1\ti8xp\main.asm >>temp.z80
+tasm -80 -i -b temp.z80 ht.bin
+if errorlevel 1 goto ERRORS
+devpac83 ht.bin
+copy ht.83p houston.8xp >nul
+del ht.83p > nul
+del ht.bin > nul
+goto DONE
+
+:83P
+echo #define TI83 >temp.z80
+type Housto~1\ti83\main.asm >>temp.z80
+tasm -80 -i -b temp.z80 houston.bin
+if errorlevel 1 goto ERRORS
+devpac83 houston
+del houston.bin >nul
+echo ----- Success!
+goto DONE
+:ERRORS
+echo ----- There were errors.
+pause
+:DONE
+del temp.lst >nul
+del temp.z80 >nul
+:DDONE
--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ fi
 if [ $readchar = "y" ]
 then 
 # Change the path below to point to your HoustonTracker directory relative to DOSBox' C: drive
-dosbox -c "set target=ti82" -c "cd texasi~1\houston\htgit\Housto~1" -exit "as.bat"
-rm ti82/MAIN.OBJ
-tilem2 ti82/MAIN.82P
+dosbox -c "cd texasi~1\houston\htgit\Housto~1" -c "set target=ti82" -exit "as.bat"
+#rm ti82/MAIN.OBJ
+tilem2 ../HOUSTON.82P
 fi
 echo -n "Build for TI-83? (y/n/q) "
 stty cbreak
@@ -29,7 +29,7 @@ if [ $readchar = "y" ]
 then 
 # Change the path below to point to your HoustonTracker directory relative to DOSBox' C: drive
 dosbox -c "cd texasi~1\houston\htgit\Housto~1" -c "set target=ti83" -exit "as.bat"
-tilem2 ti83/MAIN.83P
+tilem2 ../HOUSTON.83P
 fi
 echo -n "Build for TI-83+/84+? (y/n/q) "
 stty cbreak
@@ -44,8 +44,7 @@ if [ $readchar = "y" ]
 then 
 # Change the path below to point to your HoustonTracker directory relative to DOSBox' C: drive
 dosbox -c "cd texasi~1\houston\htgit\Housto~1" -c "set target=ti8xp" -exit "as.bat"
-rm ti8xp/MAIN.83P
-tilem2 ti8xp/MAIN.8XP
+tilem2 ../HOUSTON.8XP
 fi
 echo -n "Build for TI-73? (y/n/q) "
 stty cbreak
@@ -60,7 +59,6 @@ if [ $readchar = "y" ]
 then 
 # Change the path below to point to your HoustonTracker directory relative to DOSBox' C: drive
 dosbox -c "cd texasi~1\houston\htgit\Housto~1" -c "set target=ti73" -exit "as.bat"
-rm ti73/MAIN.BIN
 rm ti73/MAIN.LST
-tilem2 ti73/MAIN.73P
+tilem2 ../HOUSTON.73P
 fi
